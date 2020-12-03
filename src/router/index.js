7a0529464364068a1fe1af8c0f2ba25a04c4e6ef
Yiling Zhou <pastgift@gmail.com>
2020-12-03 23:01:30 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
     component: Home
   },
   {
+    path: '/doc',
+    name: 'Doc',
+    component: () => import('../views/Doc.vue'),
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
