12a674e3455308481e3926cc660ecbcd108f6a8c
Yiling Zhou <pastgift@gmail.com>
2021-03-29 19:10:58 +0800

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,8 +6,8 @@
       <router-link to="/">主页</router-link>
       | <router-link :to="{ name: 'MarkdownViewer', query: { q: 'README.md' }}">快速开始</router-link>
       | <a href="https://t.dataflux.cn/func-user-guide" target="_blank">用户手册（PDF）</a>
-      | <router-link :to="{ name: 'MarkdownViewer', query: { q: 'docs/maintenance-guide.md' }}">维护手册</router-link>
-      | <router-link :to="{ name: 'MarkdownViewer', query: { q: 'docs/development-guide.md' }}">开发手册</router-link>
+      | <router-link :to="{ name: 'MarkdownViewer', query: { q: 'maintenance-guide.md' }}">维护手册</router-link>
+      | <router-link :to="{ name: 'MarkdownViewer', query: { q: 'development-guide.md' }}">开发手册</router-link>
     </div>
   </div>
 </template>
